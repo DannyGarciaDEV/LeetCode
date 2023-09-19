6688ed5b59e88babce8a2b3da55a187498dc3acc
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-09-19 12:54:06 -0400

    Binary Tree Inorder Traversal iterative

---
+++ Binary Tree Inorder Traversal.js
@@ -0,0 +1,32 @@
+/*Input: root = [1,null,2,3]
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+*/
+
+
+var inorderTraversal = function(root) {
+    let result = []
+    let stack = []
+    while(currentNode || stack.length){
+        if(currentNode){
+            stack.push(currentNode)
+            currentNode = currentNode.left
+        }
+        else{
+            currentNode = stack.pop()
+            result.push(currentNode.val)
+            currentNode = currentNode.right
+        }
+
+        }
+        return result
+    }
